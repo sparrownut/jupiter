b43a715fa41d80570cb4f94df9c09f8d9bfb2ba4
stupidfish <2928109164@qq.com>
2023-07-23 22:11:44 +0800

    SSL修复

--- main.py
+++ main.py
@@ -33,52 +33,42 @@ if __name__ == '__main__':
                     # 将消息ID列表转换为逗号分隔的字符串
                     messages = messages[0].split(b' ')
                     sender = ""
-                    try:
-                        with eventlet.Timeout(10, False):
-                            # 循环处理每个未读邮件
-                            # print(messages)
-                            if messages[0] != b'':
-                                for msg_id in messages:
-                                    # 获取邮件内容
-                                    _, msg_data = imap.fetch(msg_id, '(RFC822)')
-                                    msg = message_from_bytes(msg_data[0][1])
-                                    for it in str(msg_data[0]).split("\\r"):
+                    with eventlet.Timeout(10, False):
+                        # 循环处理每个未读邮件
+                        # print(messages)
+                        if messages[0] != b'':
+                            for msg_id in messages:
+                                # 获取邮件内容
+                                _, msg_data = imap.fetch(msg_id, '(RFC822)')
+                                msg = message_from_bytes(msg_data[0][1])
+                                for it in str(msg_data[0]).split("\\r"):
+                                    # print(it)
+                                    if "From" in it:
                                         # print(it)
-                                        if "From" in it:
-                                            # print(it)
-                                            s = re.findall("<(.*?)>", it)
-                                            if len(s) > 0:
-                                                # print(s[0])
-                                                sender = s[0]
+                                        s = re.findall("<(.*?)>", it)
+                                        if len(s) > 0:
+                                            # print(s[0])
+                                            sender = s[0]
 
-                                    # 获取邮件正文
-                                    body = ''
-                                    if msg.is_multipart():
-                                        for part in msg.walk():
-                                            content_type = part.get_content_type()
-                                            if content_type == 'text/plain' or content_type == 'text/html':
-                                                body = part.get_payload(decode=True).decode(encoding="gb18030")
-                                                break
-                                    else:
-                                        body = msg.get_payload(decode=True).decode(encoding="gb18030")
+                                # 获取邮件正文
+                                body = ''
+                                if msg.is_multipart():
+                                    for part in msg.walk():
+                                        content_type = part.get_content_type()
+                                        if content_type == 'text/plain' or content_type == 'text/html':
+                                            body = part.get_payload(decode=True).decode(encoding="gb18030")
+                                            break
+                                else:
+                                    body = msg.get_payload(decode=True).decode(encoding="gb18030")
 
-                                    # 调用do函数并传递发件人和邮件内容参数
-                                    threading.Thread(target=do, args=(sender, body,)).start()
-                    except Exception as e:
-                        mark_all_as_read(username, password, "imap.qq.com")
-                        traceback.print_exception(e)
-
-                        imap.logout() # 重连
-                        imap = imaplib.IMAP4_SSL('imap.qq.com')
-                        imap.login(username, password)
+                                # 调用do函数并传递发件人和邮件内容参数
+                                threading.Thread(target=do, args=(sender, body,)).start()
                 except Exception as e:
                     mark_all_as_read(username, password, "imap.qq.com")
+                    print('- 应用层bug')
                     if "FETCH" not in str(e):
                         traceback.print_exception(e)
-
-                        imap.logout() # 重连
-                        imap = imaplib.IMAP4_SSL('imap.qq.com')
-                        imap.login(username, password)
+                    break
 
                 time.sleep(10)
                 # 等待新邮件
@@ -88,4 +78,5 @@ if __name__ == '__main__':
             imap.close()
             imap.logout()
         except Exception as e:
+            print('- 登录层BUG')
             traceback.print_exception(e)
